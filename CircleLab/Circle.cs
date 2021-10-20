4a105387fcde0ddb0abc140e55315f5faf171020
areddy1 <AnuragReddy@rocketmortgage.com>
2021-10-20 07:54:23 -0400

    Circle Lab

---
+++ CircleLab/Circle.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+
+namespace CircleLab
+{
+    class Circle
+    {
+        public double Radius { get; set; }
+        
+
+        public Circle (double Radius)
+        {
+            this.Radius = Radius;
+            
+        }
+
+        public double CalculateCircumference 
+        {
+            get 
+             {
+           
+            return 2 * Math.PI * Radius; 
+
+            }
+
+        }
+
+        public double CalculateArea
+        {
+            get
+            {
+                return Math.PI * Radius * Radius; 
+            }
+
+
+
+
+        }
+
+        public string GetUserInput(string prompt)
+        {
+            Console.WriteLine(prompt);
+            string input = Console.ReadLine();
+
+            return input;
+        }
+
+        public bool Continue()
+        {
+
+            string answer = GetUserInput("Would you like to continue? y/n");
+            if (answer.ToLower() == "y")
+            {
+                return true;
+            }
+            else if (answer.ToLower() == "n")
+            {
+                Console.WriteLine("Goodbye!");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("I'm sorry I didn't understand");
+                Console.WriteLine("Let's try again.");
+
+                // This is recursion, calling a method inside itself
+                return Continue();
+            }
+
+        }
+        }
+    }
+
+
+            // Misc Notes: 
+
+
+            //double radius;
+            //double PI = 3.142;
+            //double Cir, dia;
+            //Console.WriteLine("Enter the Radius of the Circle");
+            //radius = double.Parse(Console.ReadLine());
+            //Cir = 2 * PI * radius;
+            //dia = 2 * PI;
+            //return $"The circumfernce is {Cir} and diameter is {dia}";
---
+++ CircleLab/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CircleLab
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+            bool goOn = true; 
+
+            while(goOn == true)
+            {
+                Console.WriteLine("Welcome to the Circle Tester");
+                Console.WriteLine("What is the circle's radius: ");
+                double radius = Convert.ToDouble((Console.ReadLine()));
+                Circle ans = new Circle(radius);
+
+                Console.WriteLine($"The radius is: {radius}");
+                Console.WriteLine($"The Circumference of the circle is {ans.CalculateCircumference}");
+                Console.WriteLine($"The Area of the circle is {ans.CalculateArea}");
+                
+
+
+               goOn = ans.Continue(); 
+            
+
+            }
+        }
+
+       
+        
+        }
+    }
